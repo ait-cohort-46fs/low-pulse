694ca4ff08219a488134174c7b05d04d5c5e7d21
edward <edwardlfish@gmail.com>
2024-11-02 12:38:17 +0200

    Receive from eddpulsedata

--- src/main/java/ait/cohort46/pulse/service/PulseService.java
+++ src/main/java/ait/cohort46/pulse/service/PulseService.java
@@ -1,6 +1,7 @@
 package ait.cohort46.pulse.service;
 
 import ait.cohort46.pulse.dto.PulseDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
@@ -8,11 +9,15 @@ import java.util.function.Consumer;
 
 @Service
 public class PulseService {
+    @Value("${pulse.min}")
+    private int minPulse;
+
     @Bean
     public Consumer<PulseDto> receiveData() {
         return p -> {
-            long delay = System.currentTimeMillis() - p.getTimestamp();
-            System.out.println("delay: " + delay + ", id: " + p.getId() + ", pulse: " + p.getPayload());
-        };
+            if (p.getPayload() < minPulse) {
+                long delay = System.currentTimeMillis() - p.getTimestamp();
+                System.out.println("delay: " + delay + ", id: " + p.getId() + ", pulse: " + p.getPayload());
+            }        };
     }
 }
